b9818b94f82648996608b3d4d6fe7cfddd41ae4f
“Hayato <hayabusa06mochi@gmail.com>
2023-01-05 17:36:54 +0900

    基本実装完了

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+use App\Http\Requests\TodoRequest;
+
+class TodoController extends Controller
+{
+    public function index()
+    {
+        $todos = Todo::all();
+        return view('index', ['mochi' => $todos]);
+    }
+    public function create(TodoRequest $request)
+    {
+        $form = $request->all();
+        unset($form['_token']);
+        Todo::create($form);
+        // ここのcreateは意味のあるcreate
+        return redirect('/');
+    }
+    public function update(TodoRequest $request)
+    {
+        $form = $request->all();
+        unset($form['_token']);
+        Todo::find($request->id)->update($form);
+        return redirect('/');
+    }
+    public function delete(Request $request)
+    {
+        Todo::find($request->id)->delete();
+        return redirect('/');
+    }
+}
